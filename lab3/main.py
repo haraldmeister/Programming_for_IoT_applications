c308c23a32b8f708327110e9df5897a47762de61
haraldmeister <edo.fassio@gmail.com>
2018-11-15 12:56:40 +0100

    Finished first exercise

--- lab3/main.py
+++ lab3/main.py
@@ -2,10 +2,15 @@ import requests
 import json
 while True:
     a=input("Give me the first operand\n")
-    operand1=float(a)
     b=input("Give me the second operand\n")
-    operand2=float(b)
     c=input("Operation needed to do with the two operands\n")
+
+    if a=="quit" or b=="quit" or c=="quit":
+        print("Quitting program...")
+        break
+
+    operand1=float(a)
+    operand2=float(b)
 
     if c=='addiction':
         result=requests.get("http://localhost:9090/add"+"?&op1="+a+"&op2="+b).json()
@@ -19,6 +24,7 @@ while True:
     elif c=='division':
         result=requests.get("http://localhost:9090/div"+"?&op1="+a+"&op2="+b).json()
         #json_data=json.loads(result)
+
 
     print("Operand 1 = ", result["operand1"])
     print("Operand 2 = ", result["operand2"])
